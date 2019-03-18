06bd68c940088d5c7544939c6392e64938843318
pheromone <510738319@qq.com>
2019-03-18 22:25:49 +0800

    全局随机更改主题色
    全局随机更改主题色

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 enum CounterEvent { changeTheme }
 
@@ -11,13 +12,16 @@ class CounterBloc extends Bloc<CounterEvent, Map> {
   Stream<Map> mapEventToState(Map initialState, CounterEvent event) async* {
     switch (event) {
       case CounterEvent.changeTheme:
-        print('++++++++++++++____________++++++++');
-
+        var r = Random().nextInt(255) ;
+        var g = Random().nextInt(255) ;
+        var b = Random().nextInt(255) ;
+    
+        var color =Color.fromRGBO(r, g, b, 1.0);
         if (initialState['fontFamily'] == 'customFont') {
-          yield initialState = {'color': Colors.blueAccent, 'fontFamily': ''};
+          yield initialState = {'color': color, 'fontFamily': ''};
         } else {
           yield initialState = {
-            'color': Colors.pink,
+            'color': color,
             'fontFamily': 'customFont'
           };
         }
--- lib/pages/audio_page.dart
+++ lib/pages/audio_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/counter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // UI适配库
+
 
 class AudioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blueAccent
-      ),
+      theme: ThemeData(primaryColor: Colors.blueAccent),
       debugShowCheckedModeBanner: false, // 去除debug旗标
       home: MainAudio(),
     );
@@ -16,7 +16,6 @@ class AudioPage extends StatelessWidget {
 }
 
 class MainAudio extends StatefulWidget {
-
   @override
   _MainAudioState createState() => _MainAudioState();
 }
@@ -25,35 +24,36 @@ class _MainAudioState extends State<MainAudio> {
   @override
   Widget build(BuildContext context) {
     final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
-    return Scaffold(
-      appBar: PreferredSize(
-        child: Container(
-          child: AppBar(
-            title: Text('音频'),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.blueAccent,
-                Colors.white,
-              ],
+    return BlocBuilder(
+      bloc: _counterBloc,
+      builder: (BuildContext context, Map theme) {
+        return Scaffold(
+            appBar: PreferredSize(
+              child: Container(
+                child: AppBar(
+                  title: Text('音频',
+                  style: TextStyle(
+                      fontFamily: '${theme['fontFamily']}',
+                      fontSize: ScreenUtil().setSp(60)),
+                  ),
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                ),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      theme['color'],
+                      Colors.white,
+                    ],
+                  ),
+                ),
+              ),
+              preferredSize: Size(MediaQuery.of(context).size.width, 45),
             ),
-          ),
-        ),
-        preferredSize: Size(MediaQuery.of(context).size.width, 45),
-      ),
-      body: BlocBuilder<CounterEvent, Map>(
-        bloc: _counterBloc,
-        builder: (BuildContext context, Map theme){
-          return Center(
-             child: Text('$theme'), 
-          );
-        },
-      ),
+            body: Text('音频'));
+      },
     );
   }
 }
